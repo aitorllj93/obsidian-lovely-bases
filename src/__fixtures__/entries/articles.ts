6a696f29d67d0e1c7667f5d5644a1f877afcd2f2
aitorllamas <aitorllj93@gmail.com>
2026-01-14 05:41:51 +0100

    add some fixtures

--- src/__fixtures__/entries/articles.ts
+++ src/__fixtures__/entries/articles.ts
@@ -13,8 +13,8 @@ export const ARTICLE_ENTRIES: BasesEntry[] = [
       title: "‘Tienes que encontrar aquello que amas’, dice Jobs",
       excerpt: "Este es un texto preparado del discurso de graduación pronunciado por Steve Jobs, director ejecutivo de Apple Computer y de Pixar Animation Studios, el 12 de junio de 2005",
       author: [
+        "Steve Jobs",
         "Stanford University",
-        "Jobs, Steve",
       ],
       banner: 'https://news.stanford.edu/__data/assets/image/0028/165169/050609-228.jpg',
       url: 'https://news.stanford.edu/stories/2005/06/youve-got-find-love-jobs-says',
@@ -22,6 +22,100 @@ export const ARTICLE_ENTRIES: BasesEntry[] = [
       published: 'June 12th',
     }
   ),
+  aBasesEntry(
+    {
+      file: aFile({
+        basename: "5 plug-ins to supercharge mind mapping in Obsidian Canvas",
+      }),
+    },
+    {
+      title: "5 plug-ins to supercharge mind mapping in Obsidian Canvas",
+      excerpt: "Discover the top 5 plug-ins that will revolutionize your mind mapping experience in Obsidian Canvas, making your workflow more efficient and your maps more visually appealing.",
+      author: ["Tom Li"],
+      banner: "https://static0.xdaimages.com/wordpress/wp-content/uploads/wm/2025/08/xda-laptop-running-obsidian-feature.jpg?w=1600&h=900&fit=crop",
+      url: "https://www.xda-developers.com/best-obsidian-canvas-plugins/",
+      published: "2025-08-15",
+    }
+  ),
+  aBasesEntry(
+    {
+      file: aFile({
+        basename: "10 Prompts That Makes ChatGPT Write like a Human - Prompts",
+      })
+    },
+    {
+      "title": "10 Prompts That Makes ChatGPT Write like a Human - Prompts",
+      "excerpt": "Discover 10 powerful prompts to make ChatGPT write more like a human. Learn how to use conversational tones, personal stories, emotions, and more to create engaging and relatable content that connects with your audience.",
+      "banner": "https://cdn.prod.website-files.com/64808e3805a22fc1ca46ffe9/671de0c11e075a7268aebaf1_Prompts%20That%20Makes%20Chatgpt%20Write%20like%20a%20Human.webp",
+      "url": "https://www.godofprompt.ai/blog/prompts-that-makes-chatgpt-write-like-a-human",
+    }
+  ),
+  aBasesEntry(
+    {
+      file: aFile({
+        basename: "The Ultimate Guide to Mind Mapping in Obsidian",
+      })
+    },{
+      "title": "Brad Woods Digital Garden",
+      "excerpt": "What is Juice in software development. What is Game Feel and how it can it be used in non-game software. How software can fulfil emotional requirements. How to create software with soul. Examples of Juice on the web.",
+      "url": "https://garden.bradwoods.io/notes/design/juice",
+      "author": [
+        "Brad Woods"
+      ],
+      "banner": "https://garden.bradwoods.io/ogImage.jpg\t",
+      "published": "2023-03-12"
+    }
+  ),
+  aBasesEntry(
+    {
+      file: aFile({
+        basename: "40 preguntas para hacerte cada año",
+      })
+    },
+    {
+      title: "40 preguntas para hacerte cada año",
+      excerpt: "Cada año me hago estas 40 preguntas. He compartido esta lista con mi familia y mis amigos más cercanos, y siempre disfruto comentarlas mientras reflexionamos sobre…",
+      author: [
+        "Ango, Steph"
+      ],
+      banner: "https://stephango.com/assets/covers/40-questions.png",
+      url: "https://stephango.com/40-questions",
+      published: "2016-10-20"
+    }
+  ),
+  aBasesEntry(
+    {
+      file: aFile({
+        basename: "Enseñé este sistema de productividad a 6,642 empleados de Google",
+      })
+    },
+    {
+      "title": "Enseñé este sistema de productividad a 6,642 empleados de Google",
+      "excerpt": "Hey friends - During my nine years at Google, I taught a productivity system to over 6,600 Googlers that completely changed how they work.It’s a simple, 4-step workflow called CORE, and it ensures you never lose another file, task, or note again, without having to rely on",
+      "author": [
+        "Jeff Su"
+      ],
+      "banner": "https://www.jeffsu.org/content/images/2021/03/RANDOM-PICS--2-.png",
+      "url": "https://www.jeffsu.org/i-taught-6-642-googlers-this-productivity-system/",
+      "published": "2025-10-21",
+    }
+  ),
+  aBasesEntry(
+    {
+      file: aFile({
+        basename: "Las explicaciones concisas aceleran el progreso",
+      })
+    },
+    {
+      "excerpt": "Si quieres progresar más rápido, escribe explicaciones concisas. Expón las ideas en términos simples, con firmeza y claridad, de modo que puedan ser rebatidas, remezcladas o reelaboradas.",
+      "author": [
+        "Steph Ango"
+      ],
+      "banner": "https://stephango.com/assets/covers/concise.png",
+      "url": "https://stephango.com/concise",
+      "published": "2023-08-20",
+    }
+  )
 ];
 
 export const VIRTUAL_SCROLL_ARTICLES_ENTRIES: BasesEntry[] = []
--- src/__fixtures__/entries/books.ts
+++ src/__fixtures__/entries/books.ts
@@ -5,6 +5,48 @@ import { aFile } from "../../__mocks__/aFile";
 
 export const BOOK_ENTRIES: BasesEntry[] = [
   aBasesEntry(
+
+    {
+      file: aFile({
+        basename: 'Steve Jobs',
+      })
+    },
+    {
+      "title": "Steve Jobs",
+      "author": [
+        "Walter Isaacson"
+      ],
+      "cover": "http://books.google.com/books/content?id=ZNf9gNsA72MC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    }
+  ),
+  aBasesEntry(
+    {
+      file: aFile({
+        basename: 'Steve Jobs',
+      })
+    },{
+      "title": "Organízate con Eficacia",
+      "author": [
+        "David Allen"
+      ],
+      "cover": "https://m.media-amazon.com/images/I/61Loe8brBXL._AC_UF1000,1000_QL80_.jpg",
+    }
+  ),
+  aBasesEntry(
+    {
+      file: aFile({
+        basename: 'Organízate con Eficacia',
+      })
+    },
+    {
+      "title": "Calendario De La Sabiduria",
+      "author": [
+        "León Tolstói"
+      ],
+      "cover": "https://www.sbooks.es/imagenes/poridentidad?identidad=c97565fb-c4d0-461e-9d89-f2949458acaa&ancho=&alto="
+    }
+  ),
+  aBasesEntry(
     {
       file: aFile({
         basename: 'La divina comedia',
@@ -12,12 +54,10 @@ export const BOOK_ENTRIES: BasesEntry[] = [
     },
     {
       title: "La divina comedia",
-      status: "Pendiente",
       author: [
         "Dante Alighieri",
       ],
       cover: 'https://m.media-amazon.com/images/I/71WJbXGxPdL._AC_UF894,1000_QL80_.jpg',
-      url: 'https://news.stanford.edu/stories/2005/06/youve-got-find-love-jobs-says',
     }
   ),
 ];
